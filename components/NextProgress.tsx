a40712b78dd32689d372214e8866feb62a0a2f7f
cullyfung <cullyfung@gamil.com>
2023-11-11 10:45:03 +0800

    chore: remove NextProgress

--- components/NextProgress.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import NextNProgress, { NextNProgressProps } from 'nextjs-progressbar';
-
-export default function NextProgress(props: NextNProgressProps) {
-  return <NextNProgress {...props} />;
-}
