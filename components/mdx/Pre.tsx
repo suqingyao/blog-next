f863a4bdedbea86b19691a2ab9dc3a9d90492074
cullyfung <cullyfung@gamil.com>
2023-11-28 01:30:55 +0800

    fix: copy error

--- components/mdx/Pre.tsx
+++ components/mdx/Pre.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import ReactDOMServer from 'react-dom/server';
+import ReactDOM from 'react-dom/client';
 import { useTheme } from 'next-themes';
 import { BiCopy } from 'react-icons/bi';
 
@@ -14,22 +14,33 @@ const Pre = ({ children, ...props }: any) => {
   const [show, setShow] = useState(false);
   const { isSupported, text, copy } = useClipboard();
 
+  async function simpleRenderToString(element: React.ReactElement) {
+    const container = document.createElement('div');
+    const root = ReactDOM.createRoot(container);
+    root.render(element);
+
+    return new Promise<string>((resolve) => {
+      setTimeout(() => {
+        resolve(container.innerHTML);
+      }, 100);
+    });
+  }
+
   const handleCopy = async () => {
     try {
       // 将 ReactNode 转换为 HTML 字符串
-      const htmlString = ReactDOMServer.renderToStaticMarkup(props.children);
+      const htmlString = await simpleRenderToString(children);
       // 使用 DOMParser 将 HTML 字符串解析为 DOM 对象
       const domParser = new DOMParser();
       const dom = domParser.parseFromString(htmlString, 'text/html');
       // 提取文本内容
       const textContent = dom.body.textContent;
       // 复制文本内容到剪贴板
-      // await navigator.clipboard.writeText(textContent!);
-      copy(textContent!);
+      await copy(textContent!);
       toast.success('Copy Success!');
     } catch (error) {
       toast.error('Failed copy');
-      console.error('Failed to copy text');
+      console.error('Failed to copy text', (error as Error).message);
     }
   };
 
