8380436a0f383cdd88239b795f9d4264c758c5f9
cullyfung <cullyfung@gmail.com>
2024-01-20 23:44:00 +0800

    style: improve social style

--- components/Social.tsx
+++ components/Social.tsx
@@ -2,44 +2,92 @@
 
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import { IconType } from 'react-icons';
 
 import { FaXTwitter, FaGithub } from 'react-icons/fa6';
 import { IoLogoWechat, IoMail } from 'react-icons/io5';
 
+type LinkText = {
+  text: string;
+  icon: IconType;
+  href?: string;
+};
+
 export default function Social() {
+  const linkText: LinkText[] = [
+    {
+      text: 'https://twitter.com/cully_fung',
+      href: 'https://twitter.com/cully_fung',
+      icon: (props) => <FaXTwitter {...props} />
+    },
+    {
+      text: 'https://github.com/cullyfung',
+      href: 'https://github.com/cullyfung',
+      icon: (props) => <FaGithub {...props} />
+    },
+    {
+      text: 'cullyfung',
+      icon: (props) => <IoLogoWechat {...props} />
+    },
+    {
+      text: 'cullyfung@gmail.com',
+      icon: (props) => <IoMail {...props} />
+    }
+  ];
+
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 1, duration: 1 }}
+    <motion.ul
+      initial="initial"
+      animate="animate"
+      variants={{
+        initial: {
+          transition: {
+            when: 'afterChildren'
+          }
+        },
+        animate: {
+          transition: {
+            when: 'beforeChildren',
+            staggerChildren: 0.3,
+            transition: {
+              ease: 'easeInOut'
+            }
+          }
+        }
+      }}
       className="flex flex-col gap-5"
     >
-      <div className="flex items-center">
-        <FaXTwitter size={24} />
-        <Link
-          href="https://twitter.com/cully_fung"
-          className="text-link ml-2"
+      {linkText.map(({ href, text, icon: Icon }, index) => (
+        <motion.li
+          key={index}
+          variants={{
+            initial: {
+              opacity: 0
+            },
+            animate: {
+              opacity: 1,
+              transition: {
+                ease: 'easeInOut',
+                delay: index * 0.06
+              }
+            }
+          }}
+          className="flex items-center"
         >
-          https://twitter.com/cully_fung
-        </Link>
-      </div>
-      <div className="flex items-center">
-        <FaGithub size={24} />
-        <Link
-          href="https://github.com/cullyfung"
-          className="text-link ml-2"
-        >
-          https://github.com/cullyfung
-        </Link>
-      </div>
-      <div className="flex items-center">
-        <IoLogoWechat size={24} />
-        <span className="text-link ml-2">cullyfung</span>
-      </div>
-      <div className="flex items-center">
-        <IoMail size={24} />
-        <span className="text-link ml-2">cullyfung@gmail.com</span>
-      </div>
-    </motion.div>
+          <Icon size={24} />
+          {href ? (
+            <Link
+              href={href}
+              target="_blank"
+              className="text-link ml-2"
+            >
+              {text}
+            </Link>
+          ) : (
+            <span className="text-link ml-2">{text}</span>
+          )}
+        </motion.li>
+      ))}
+    </motion.ul>
   );
 }
