65ddfef5b28692589ec8f94f6b42821a9fa9f074
suqingyao <suqingyao333@gmail.com>
2026-01-09 23:31:20 +0800

    feat(lib): add ExifTool manager for metadata parsing

---
+++ src/lib/exiftool.ts
@@ -0,0 +1,42 @@
+import { isExiftoolLoadedAtom } from '@/store/atoms/app';
+
+import { jotaiStore } from './jotai';
+
+class ExifToolManagerStatic {
+  private isLoaded = false;
+
+  private exifTool: typeof import('@uswriting/exiftool') | null = null;
+
+  async load() {
+    if (this.isLoaded)
+      return;
+    const exiftool = await import('@uswriting/exiftool');
+    console.info('ExifTool loaded...');
+    this.exifTool = exiftool;
+    this.isLoaded = true;
+
+    jotaiStore.set(isExiftoolLoadedAtom, true);
+  }
+
+  constructor() {
+    this.load();
+  }
+
+  async parse(buffer: Blob, filename?: string) {
+    if (!this.exifTool) {
+      await this.load();
+    }
+
+    if (!this.exifTool) {
+      throw new Error('ExifTool not loaded');
+    }
+    const metadata = await this.exifTool.parseMetadata(new File([buffer], `/afilmory/${filename}`));
+
+    if (!metadata.success) {
+      throw new Error(metadata.error);
+    }
+
+    return metadata.data;
+  }
+}
+export const ExifToolManager = new ExifToolManagerStatic();
