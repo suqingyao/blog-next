9a108e5cdd792056c0da841a333967aeb7f7da6d
suqingyao <suqingyao333@gmail.com>
2026-01-10 19:15:57 +0800

    refactor(hooks): improve useTitle hook with options and SSR support

--- src/hooks/use-title.ts
+++ src/hooks/use-title.ts
@@ -1,16 +1,29 @@
 import { useEffect, useRef } from 'react';
 import { APP_NAME } from '@/constants';
 
-const titleTemplate = `%s | ${APP_NAME}`;
-export function useTitle(title?: Nullable<string>) {
-  const currentTitleRef = useRef(document.title);
-  useEffect(() => {
-    if (!title)
-      return;
-
-    document.title = titleTemplate.replace('%s', title);
-    return () => {
-      document.title = currentTitleRef.current;
-    };
-  }, [title]);
+export interface UseTitleOptions {
+  restoreOnUnmount?: boolean;
 }
+
+const DEFAULT_USE_TITLE_OPTIONS: UseTitleOptions = {
+  restoreOnUnmount: false,
+};
+
+const titleTemplate = `%s | ${APP_NAME}`;
+function useTitlePrimitive(title: string, options: UseTitleOptions = DEFAULT_USE_TITLE_OPTIONS) {
+  const prevTitleRef = useRef(document.title);
+
+  if (document.title !== title)
+    document.title = title;
+
+  useEffect(() => {
+    if (options.restoreOnUnmount) {
+      document.title = titleTemplate.replace('%s', title);
+      return () => {
+        document.title = prevTitleRef.current;
+      };
+    }
+  }, []);
+}
+
+export const useTitle = typeof document !== 'undefined' ? useTitlePrimitive : (_title: string) => {};
