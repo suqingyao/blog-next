79158de245063635359e6d305d16cfb381a3b669
suqingyao <suqingyao333@gmail.com>
2026-01-24 12:53:38 +0800

    chore(deploy): prune docker resources before recreating containers

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@
 cd /root/workspace/blog-next
 docker compose down --remove-orphans
 docker container prune -f
+docker image prune -a -f
+docker builder prune -a -f
 docker compose pull
-docker compose up -d
+docker compose up -d --force-recreate
